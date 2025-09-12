02c81fc0b070fccb3e875ca685f022483ea62b33
Lucas Stein <lucasvieirastein@gmail.com>
2025-09-12 18:36:04 +0100

    changing main to have useful test data

--- src/main.py
+++ src/main.py
@@ -5,12 +5,15 @@ from internal_libs.category import Category
 from internal_libs.expense import Expense
 
 def main():
-    e = Expense(date.today(), "Team dinner", Category.FOOD, 16.74)
-    print(e)
-    print("---------------------------")
+    e1 = Expense(date.today(), "Tiago Anniversary", Category.FOOD, 16.74)
+    e2 = Expense(date.today(), "Borderlands 4", Category.GAMING, 69.99)
+    e3 = Expense(date.today(), "Diesel", Category.TRANSPORT, 50)
 
     db.init_db()
-    db.add_expense(e)
+    db.add_expense(e1)
+    db.add_expense(e2)
+    db.add_expense(e3)
+
     db.get_expenses()
 
 if __name__ == "__main__":
